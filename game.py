bc514c88312c585f4d077eb21c2ee230aa8692e1
drako9159 <chochito9159@gmail.com>
2024-12-18 16:16:04 -0600

    feat(font): font super_pixel added

--- game.py
+++ game.py
@@ -6,8 +6,11 @@ pygame.init()
 HEIGHT = 750
 WIDTH = 480
 
+# Version
+VERSION = "1.0.0"
+
 # Title Game
-TITLE = "Catcher Game"
+TITLE = "Catcher Game " + VERSION
 
 # Colors
 BLACK = (0, 0, 0)
@@ -23,6 +26,26 @@ FPS = 60
 apple_image = pygame.image.load("sprites/apple.png")
 basket_image = pygame.image.load("sprites/basket.png")
 
+# Minimal rectangle
+apple_rect = apple_image.get_rect()
+basket_rect = basket_image.get_rect()
+
+apple_rect.center = (WIDTH // 2, HEIGHT // 2)
+
+apple_rect.size = (WIDTH // 10, HEIGHT // 10)
+
+
+
+
+apple_rect.left = 10
+apple_rect.bottom = HEIGHT - 10
+
+
+basket_rect.center = (WIDTH // 2, HEIGHT // 2)
+
+
+
+
 display = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption(TITLE)
 
@@ -34,7 +57,12 @@ while not game_over:
             game_over = True
 
     display.fill(BLACK)
+
+    display.blit(apple_image,apple_rect)
+    display.blit(basket_image,basket_rect)
+    
     pygame.display.update()
+   
 
 pygame.quit()
 exit()
