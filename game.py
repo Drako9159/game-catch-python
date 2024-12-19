7ed91b2200f3f275a56b48d9caa10d00518dedb6
drako9159 <chochito9159@gmail.com>
2024-12-18 20:40:33 -0600

    feat(beta): complete update version

--- game.py
+++ game.py
@@ -18,10 +18,14 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 # Stats Game
-STUDENT_STATING_LIVES = 5
-STUDENT_VELOCITY = 15
+BASKET_STATING_LIVES = 5
+BASKET_VELOCITY = 15
 APPLE_STARTING_VELOCITY = 5
 APPLE_ACCELERATION = 0.5
+BASKET_ACCELERATION = 0.5
+
+APPLE_WITH, APPLE_HEIGHT = 50, 50
+BASKET_WIDTH, BASKET_HEIGHT = 120, 120
 
 # FPS
 FPS = 60
@@ -31,13 +35,14 @@ clock = pygame.time.Clock()
 
 # Score
 score = 0
-player_lives = STUDENT_STATING_LIVES
+player_lives = BASKET_STATING_LIVES
 apple_velocity = APPLE_STARTING_VELOCITY
+basket_velocity = BASKET_VELOCITY
 
 # Fonts
-font_title_32 = pygame.font.Font("fonts/super_pixel.ttf", 32)
-font_title_42 = pygame.font.Font("fonts/super_pixel.ttf", 42)
-font_text = pygame.font.Font("fonts/super_pixel.ttf", 24)
+font_title_32 = pygame.font.Font("fonts/spenbeb.otf", 32)
+font_title_42 = pygame.font.Font("fonts/spenbeb.otf", 42)
+font_text = pygame.font.Font("fonts/spenbeb.otf", 24)
 
 # Score
 score_text = font_text.render(f"Score: {score}", True, WHITE, BLACK)
@@ -53,28 +58,28 @@ title_rect.y = 15
 # Set Lives
 lives_text = font_text.render(f"Lives: {player_lives}", True, WHITE, BLACK)
 lives_rect = lives_text.get_rect()
-lives_rect.topright = (WIDTH - 10, 98)
+lives_rect.topright = (WIDTH - 10, 90)
 
 # Game Over Title
-game_over_title = font_title_32.render("Game Over", True, WHITE, BLACK)
-game_over_rect = game_over_title.get_rect()
-game_over_rect.center = (WIDTH // 2, HEIGHT // 2)
+gameover_title = font_title_42.render("Game Over", True, WHITE, BLACK)
+gameover_title_rect = gameover_title.get_rect()
+gameover_title_rect.center = (WIDTH // 2, HEIGHT // 2)
 
 # Game Over Text
-game_over_text = font_text.render("Press any key to play again", True, WHITE, BLACK)
-game_over_rect = game_over_text.get_rect()
-game_over_rect.center = (WIDTH // 2, (HEIGHT // 2) + 60)
+gameover_text = font_text.render("Press any key to play again", True, WHITE, BLACK)
+gameover_text_rect = gameover_text.get_rect()
+gameover_text_rect.center = (WIDTH // 2, (HEIGHT // 2) + 60)
 
 # Sprite basket
 basket_image = pygame.image.load("sprites/basket.png")
-basket_image = pygame.transform.smoothscale(basket_image, (100, 100))
+basket_image = pygame.transform.smoothscale(basket_image, (BASKET_WIDTH, BASKET_HEIGHT))
 basket_rect = basket_image.get_rect()
 basket_rect.left = 10
 basket_rect.bottom = HEIGHT - 10
 
 # Sprite apple
 apple_image = pygame.image.load("sprites/apple.png")
-apple_image = pygame.transform.smoothscale(apple_image, (50, 50))
+apple_image = pygame.transform.smoothscale(apple_image, (APPLE_WITH, APPLE_HEIGHT))
 apple_rect = apple_image.get_rect()
 apple_rect.left = random.randint(0, WIDTH - 64)
 apple_rect.bottom = HEIGHT // 2 - 175
@@ -91,18 +96,32 @@ while not game_over:
         if event.type == pygame.QUIT:
             game_over = True
 
+    # Keyboard
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and basket_rect.left > 0:
-        basket_rect.left -= STUDENT_VELOCITY
+        basket_rect.left -= basket_velocity
     if keys[pygame.K_RIGHT] and basket_rect.right < WIDTH:
-        basket_rect.right += STUDENT_VELOCITY
+        basket_rect.right += basket_velocity
     if keys[pygame.K_UP] and basket_rect.top > 150:
-        basket_rect.top -= STUDENT_VELOCITY
+        basket_rect.top -= basket_velocity
     if keys[pygame.K_DOWN] and basket_rect.bottom < HEIGHT:
-        basket_rect.bottom += STUDENT_VELOCITY
+        basket_rect.bottom += basket_velocity
 
     # Apple
-    
+    if apple_rect.y > HEIGHT:
+        apple_rect.x = random.randint(0, WIDTH - 64)
+        apple_rect.y = 140
+        player_lives -= 1
+    else:
+        apple_rect.y += apple_velocity
+
+    # Collision
+    if basket_rect.colliderect(apple_rect):
+        apple_rect.x = random.randint(0, WIDTH - 64)
+        apple_rect.y = 100
+        apple_velocity += APPLE_ACCELERATION
+        score += 1
+        basket_velocity += BASKET_ACCELERATION
 
     # Score
     score_text = font_text.render(f"Score: {score}", True, WHITE, BLACK)
@@ -114,11 +133,29 @@ while not game_over:
     display.blit(score_text, score_rect)
     display.blit(lives_text, lives_rect)
 
+    pygame.draw.line(display, WHITE, (0, 140), (WIDTH, 140), 3)
+
     display.blit(apple_image,apple_rect)
     display.blit(basket_image,basket_rect)
     
-    pygame.draw.line(display, WHITE, (0, 140), (WIDTH, 140), 3)
-    
+    if player_lives == 0:
+        display.blit(gameover_title, gameover_title_rect)
+        display.blit(gameover_text, gameover_text_rect)
+        pygame.display.update()
+        pygame.time.wait(2000)
+        is_paused = True
+        while is_paused:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    is_paused = False
+                    game_over = True
+                if event.type == pygame.KEYDOWN:
+                    is_paused = False
+                    score = 0
+                    player_lives = BASKET_STATING_LIVES
+                    apple_velocity = APPLE_STARTING_VELOCITY
+                    basket_velocity = BASKET_VELOCITY
+
     pygame.display.update()
 
     clock.tick(FPS)
